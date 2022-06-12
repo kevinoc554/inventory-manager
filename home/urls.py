a7cff2c4fb5019b549176ccbe42b4a542f826b8e
Kevin <kevinoc554@gmail.com>
2022-06-12 16:54:19 +0100

    feat: create delete view, template and url, and define logic for GET requests

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ from home import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("add/", views.add_inventory, name="add_inventory"),
+    path("delete/<int:inventory_id>", views.delete_inventory, name="delete_inventory"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 import imp
 from re import template
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Inventory
 from .forms import InventoryForm
 
@@ -30,3 +30,17 @@ def add_inventory(request):
             form.save()
     
     return redirect('home')
+
+def delete_inventory(request, inventory_id):
+    """
+    A view to delete inventory items. Asks user to confirm 
+    input as deletion cannot be undone. 
+    """
+    item = get_object_or_404(Inventory, pk=inventory_id)
+    if request.method == 'GET':
+        template = 'home/delete.html'
+        context = {
+            'item': item,
+        }
+
+        return render(request, template, context)
