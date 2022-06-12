d3202a5d66846a9d79917fc965167a2fbb557075
Kevin <kevinoc554@gmail.com>
2022-06-12 20:16:02 +0100

    feat: change inventory model and form to allow for delete/undelete and delete comments

--- home/forms.py
+++ home/forms.py
@@ -5,4 +5,4 @@ from .models import Inventory
 class InventoryForm(forms.ModelForm):
     class Meta:
         model = Inventory
-        fields = '__all__'
+        exclude = ('deleted', 'delete_comment')
--- home/models.py
+++ home/models.py
@@ -5,9 +5,11 @@ class Inventory(models.Model):
     name = models.CharField(max_length=254)
     quantity = models.IntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    deleted = models.BooleanField(default=False)
+    delete_comment = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name_plural = 'Inventory'
+        verbose_name_plural = "Inventory"
